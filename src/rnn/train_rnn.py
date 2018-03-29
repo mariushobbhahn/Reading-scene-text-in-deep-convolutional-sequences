96dbd27f9e596d489b53c12c724ec5683e138970
mariushobbhahn <marius.hobbhahn@student.uni-tuebingen.de>
2018-03-29 13:47:54 +0200

    minor changes to rnn

--- src/rnn/train_rnn.py
+++ src/rnn/train_rnn.py
@@ -16,7 +16,7 @@ from data.predicted import PredictFeatures
 from rnn.rnn_network import *
 
 class TrainRNNModel(ModelDesc):
-    def __init__(self, max_length=30):
+    def __init__(self, max_length=36):
         self.max_length = max_length
         self.input_vector_size = 128
 
@@ -25,7 +25,7 @@ class TrainRNNModel(ModelDesc):
         Define all the inputs (with type, shape, name) that
         the graph will need.
         """
-        # TODO Input is a sequence of 128D vectors and a n-length label
+
         return [InputDesc(tf.float32, (None, self.max_length, self.input_vector_size), 'input'),
                 InputDesc(tf.int32, (None,), 'label')]
 
@@ -35,14 +35,13 @@ class TrainRNNModel(ModelDesc):
 
         # inputs contains a list of input variables defined above
         features, label = inputs
-        # constant for the length of this particular sequence
-        sequence_length = len(inputs)
 
-        logits = build_rnn(inputs)
+        #build the graph
+        logits = build_rnn(features)
 
         """CTC"""
         decoded, log_probs = tf.nn.ctc_beam_search_decoder(inputs=logits,
-                                                           sequence_length=seq_length)  # log prob will not be used afterwards
+                                                           sequence_length=self.max_length)  # log prob will not be used afterwards
 
         # print the predicted labels for the first data point in each step.
         decoded = tf.Print(decoded,
